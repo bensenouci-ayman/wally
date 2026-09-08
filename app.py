aed400d40685dbe0d6acfbcaec028f69c1c851e7
Bensenouci aymen <bensenouciayman@gmail.com>
2026-09-08 17:47:10 +0200

    show categories and total Expenses in Dashboard

--- app.py
+++ app.py
@@ -24,11 +24,22 @@ with app.app_context():
     db.create_all()
 
 
+CATEGORIES = ['Food', 'Transport', 'Rent', 'Utilities', 'Health', 'Invest']
+
+
 # home decorator
 @app.route("/")
 def index():
+
     expenses = Expense.query.order_by(Expense.date.desc(), Expense.id.desc()).all()
-    return render_template("index.html", expenses=expenses)
+    total = round(sum(e.amount for e in expenses), 2)
+
+    return render_template(
+        "index.html",
+        expenses=expenses,
+        categories=CATEGORIES,
+        total=total
+        )
 
 
 # add decorator
