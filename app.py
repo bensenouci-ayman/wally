274c0fc9088c54715d41c2516d078b664ef27141
Bensenouci aymen <bensenouciayman@gmail.com>
2026-09-08 10:13:48 +0200

    Create python Expense Model (database)

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 # create instance
 app = Flask(__name__)
+
+# Database Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(120), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False, default=date.today)
+
+
+with app.app_context():
+    db.create_all()
+
 
 # home decorator
 @app.route("/")
