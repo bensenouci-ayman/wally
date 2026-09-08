5f1fc26a545e285d85f625bc18b9b961a0fa193a
Bensenouci aymen <bensenouciayman@gmail.com>
2026-09-08 10:44:15 +0200

    handle form submission (add-form) and add flash messeges and make some changes on index file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
@@ -26,6 +26,29 @@ with app.app_context():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# add decorator
+@app.route("/add", methods=['POST'])
+def add():
+    description = (request.form.get("description") or "").strip()
+    amount_str = (request.form.get("amount") or "").strip()
+    category = (request.form.get("category") or "").strip()
+    date_str = (request.form.get("date") or "").strip()
+
+    if not description or not amount_str or not category or not date_str:
+        flash("Please fill all the fields", "error")
+
+    try:
+        amount = float(amount_str)
+        if amount <= 0:
+            raise ValueError
+    except ValueError:
+        flash("Amount must be a positive number", "error")
+        return redirect(url_for("index"))
+    
+
+
 
 # additional check
 if __name__ == "__main__":
